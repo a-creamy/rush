78d64fdd2ed4fe41a1c34a926c9b32167a9c1955
A-cream <akfain@proton.me>
2025-02-01 20:17:44 +0000

    Add cd

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use libc::{c_char, geteuid, gethostname, getpwuid};
+use std::env::{self};
 use std::ffi::CStr;
 use std::io::{self, Write};
+use std::path::PathBuf;
+mod run;
 
 struct Shell {
     prompt: String,
@@ -18,7 +21,22 @@ impl Shell {
             "unknown".into()
         });
 
-        let default_prompt = format!("[{}@{}]$", username, hostname);
+        let dir = match env::current_dir() {
+            Ok(path) => path,
+            Err(e) => {
+                eprintln!("Failed to get current directory: {}", e);
+                let default_prompt = format!("[{}@{} unknown]$", username, hostname);
+                return Shell {
+                    prompt: default_prompt,
+                };
+            }
+        };
+
+        let current_dir = dir
+            .file_name()
+            .unwrap_or_else(|| dir.as_os_str())
+            .to_string_lossy();
+        let default_prompt = format!("[{}@{} {}]$", username, hostname, current_dir);
         match prompt {
             Some(p) => Shell {
                 prompt: p.to_string(),
@@ -35,6 +53,24 @@ impl Shell {
         io::stdout().flush()?;
         io::stdin().read_line(&mut s)?;
         Ok(s.trim().to_string())
+    }
+
+    fn update_dir(&mut self, updated_dir: PathBuf) {
+        let username = get_username().unwrap_or_else(|e| {
+            eprintln!("Failed to retrieve user information: {}", e);
+            "unknown".into()
+        });
+
+        let hostname = get_hostname().unwrap_or_else(|e| {
+            eprintln!("Failed to get hostname: {}", e);
+            "unknown".into()
+        });
+
+        let current_dir = updated_dir
+            .file_name()
+            .unwrap_or_else(|| updated_dir.as_os_str())
+            .to_string_lossy();
+        self.prompt = format!("[{}@{} {}]$", username, hostname, current_dir);
     }
 }
 
@@ -67,10 +103,31 @@ fn get_hostname() -> Result<String, String> {
 }
 
 fn main() {
-    let shell = Shell::new(None);
+    let mut shell = Shell::new(None);
+    let mut original_dir = match env::current_dir() {
+        Ok(path) => path,
+        Err(e) => {
+            eprintln!("Failed to get current directory: {}", e);
+            return;
+        }
+    };
+
     loop {
         match shell.input() {
-            Ok(user_input) => println!("{}", user_input),
+            Ok(user_input) => {
+                run::execute(&user_input);
+                let updated_dir = match env::current_dir() {
+                    Ok(path) => path,
+                    Err(e) => {
+                        eprintln!("Failed to get current directory: {}", e);
+                        return;
+                    }
+                };
+                if original_dir != updated_dir {
+                    shell.update_dir(updated_dir.clone());
+                    original_dir = updated_dir;
+                }
+            }
             Err(e) => eprintln!("Error reading input: {}", e),
         }
     }
