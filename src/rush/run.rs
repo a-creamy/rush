22f43eb40ec8c25ce9e42a8a6ff8673d5f6967c6
a-creamy <akfain@proton.me>
2025-02-02 12:06:41 +0000

    Added exit command

--- src/rush/run.rs
+++ src/rush/run.rs
@@ -1,68 +1,16 @@
-use std::collections::VecDeque;
-use std::io;
-use std::process::{Command, Stdio};
+mod custom;
 pub mod dir;
-
-fn tokenize(input: &str) -> VecDeque<String> {
-    let mut tokens = VecDeque::new();
-    let words: Vec<&str> = input.split_whitespace().collect();
-    for word in words {
-        if !word.is_empty() {
-            tokens.push_back(word.to_string());
-        }
-    }
-    return tokens;
-}
-
-fn command_exists(cmd: &str) -> bool {
-    Command::new("which")
-        .arg(cmd)
-        .output()
-        .map(|output| output.status.success())
-        .unwrap_or(false)
-}
-
-fn run_program(program: &str, args: &[&str]) -> io::Result<()> {
-    let mut command = Command::new(program);
-    command.args(args);
-
-    command.stdin(Stdio::inherit());
-    command.stdout(Stdio::inherit());
-    command.stderr(Stdio::inherit());
-
-    let status = command.status()?;
-
-    if !status.success() {
-        eprintln!("rush: {} exited with error: {:?}", program, status);
-    }
-
-    Ok(())
-}
+mod utils;
 
 pub fn execute(cmd: &str) {
-    let args = tokenize(cmd);
-    if args.is_empty() {
+    let args = utils::tokenize(cmd);
+    if args.is_empty() || custom::execute(&args) {
         return;
     }
 
-    if &args[0] == "cd" {
-        let arg = if args.len() > 1 {
-            Some(args[1].as_str())
-        } else {
-            None
-        };
-        match dir::cd(arg) {
-            Ok(()) => return,
-            Err(e) => {
-                eprintln!("cd: {}", e);
-                return;
-            }
-        }
-    }
-
-    if command_exists(&args[0]) {
+    if utils::command_exists(&args[0]) {
         let args_slice: Vec<&str> = args.iter().skip(1).map(|s| s.as_str()).collect();
-        let _ = run_program(&args[0], &args_slice);
+        let _ = utils::run_program(&args[0], &args_slice);
     } else {
         eprintln!("rush: {}: command not found", &args[0]);
     }
