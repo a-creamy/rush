911b2e83e408e1dcdb3c7e1663da3cfd79d2f493
a-creamy <akfain@proton.me>
2025-02-05 18:32:00 +0000

    Basic shell

---
+++ src/shell.rs
@@ -0,0 +1,50 @@
+use std::io::{self, stdin, stdout, Write};
+use std::str;
+mod dir;
+
+struct Shell {
+    prompt: String,
+}
+
+impl Shell {
+    fn new(prompt: Option<&str>) -> Shell {
+        match prompt {
+            Some(_) => Shell {
+                prompt: prompt.unwrap().to_string(),
+            },
+
+            None => match dir::current() {
+                Ok(path) => Shell {
+                    prompt: format!("{} > ", path.display()),
+                },
+                Err(e) => panic!("rush: {e}"),
+            },
+        }
+    }
+
+    fn input(&self) -> io::Result<String> {
+        print!("{}", self.prompt);
+        let mut s = String::new();
+        stdout().flush()?;
+        stdin().read_line(&mut s)?;
+        Ok(s.trim().to_string())
+    }
+
+    fn handle_dir(&mut self) {
+        self.prompt = match dir::current() {
+            Ok(path) => format!("{} > ", path.display()),
+            Err(e) => panic!("rush: {e}"),
+        }
+    }
+}
+
+pub fn run() {
+    let mut shell = Shell::new(None);
+    loop {
+        shell.handle_dir();
+        match shell.input() {
+            Ok(output) => println!("{output}"),
+            Err(e) => eprintln!("{e}"),
+        }
+    }
+}
---
+++ src/shell/dir.rs
@@ -0,0 +1,46 @@
+use libc::{getpwuid, getuid};
+use std::env;
+use std::ffi::CStr;
+use std::io::{self, Error, ErrorKind};
+use std::path::PathBuf;
+
+pub fn home_dir() -> io::Result<String> {
+    unsafe {
+        let uid = getuid();
+        let passwd = getpwuid(uid);
+
+        if passwd.is_null() {
+            return Err(Error::new(
+                ErrorKind::NotFound,
+                "Failed to get password entry",
+            ));
+        }
+
+        let home_dir = (*passwd).pw_dir;
+
+        if home_dir.is_null() {
+            return Err(Error::new(ErrorKind::NotFound, "Home directory not found"));
+        }
+
+        CStr::from_ptr(home_dir)
+            .to_str()
+            .map(String::from)
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e))
+    }
+}
+
+pub fn current() -> io::Result<PathBuf> {
+    match env::current_dir() {
+        Ok(path) => {
+            let home = PathBuf::from(home_dir()?);
+            if let Ok(relative_path) = path.strip_prefix(&home) {
+                let mut new_path = PathBuf::from("~");
+                new_path.push(relative_path);
+                return Ok(new_path);
+            }
+
+            Ok(path)
+        }
+        Err(e) => Err(e),
+    }
+}
