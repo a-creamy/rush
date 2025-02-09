4f9886092d652dda03fea131618c6fde51d1c8f4
A-cream <akfain@proton.me>
2025-02-09 12:09:05 +0000

    Support for logical and (&&)

--- src/run/parser.rs
+++ src/run/parser.rs
@@ -2,6 +2,22 @@ use crate::run::node;
 use std::path::PathBuf;
 
 pub fn parse(input: &str) -> node::AST {
+    let and_groups: Vec<&str> = input.split("&&").collect();
+    let mut commands = Vec::new();
+
+    for group in and_groups {
+        let ast = parse_single_command(group.trim());
+        commands.push(ast);
+    }
+
+    if commands.len() == 1 {
+        commands.pop().unwrap()
+    } else {
+        node::AST::AndList(commands)
+    }
+}
+
+pub fn parse_single_command(input: &str) -> node::AST {
     let mut parts = input.split_whitespace().peekable();
     let mut commands = Vec::new();
     let mut current_cmd = Vec::new();
