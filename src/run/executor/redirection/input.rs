008f08fd2078cfd73887eae42e724a539b1a547f
A-cream <akfain@proton.me>
2025-02-17 08:32:59 +0000

    '<' Input redirection

---
+++ src/run/executor/redirection/input.rs
@@ -0,0 +1,32 @@
+use crate::run::{error::ShellError, node::Ast};
+use std::{
+    fs::File,
+    process::{Command, Stdio},
+};
+
+pub fn execute(lhs: &Ast, rhs: &Ast) -> Result<(), ShellError> {
+    let args = match lhs {
+        Ast::Command(args) => args,
+        _ => {
+            return Err(ShellError::InvalidArgument(
+                "'<' Left hand side must be a command".to_string(),
+            ))
+        }
+    };
+
+    let file = match rhs {
+        Ast::Command(args) => File::open(args[0].clone())?,
+        _ => {
+            return Err(ShellError::InvalidArgument(
+                "'<' Right hand side must be a file".to_string(),
+            ))
+        }
+    };
+
+    let mut binding = Command::new(args[0].clone());
+    let cmd = binding.args(&args[1..]);
+
+    cmd.stdin(Stdio::from(file)).spawn()?.wait()?;
+
+    Ok(())
+}
