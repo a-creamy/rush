e5bdc238b0481bc9cef2a779e9927712b206a573
a-creamy <akfain@proton.me>
2025-02-17 10:52:15 +0000

    Small changes

--- src/run/executor/redirection/output.rs
+++ src/run/executor/redirection/output.rs
@@ -1,13 +1,10 @@
+use std::{fs::File, path::PathBuf, process::{Command, Stdio}, thread};
+
 use crate::run::{error::ShellError, node::Ast};
-use std::{
-    fs::File,
-    path::PathBuf,
-    process::{Command, Stdio},
-};
 
 pub fn execute(lhs: &Ast, rhs: &Ast) -> Result<(), ShellError> {
     let args = match lhs {
-        Ast::Command(args) => args,
+        Ast::Command(args) => args.clone(),
         _ => {
             return Err(ShellError::InvalidArgument(
                 "'&>' Does not support this type of command".to_string(),
@@ -27,11 +24,13 @@ pub fn execute(lhs: &Ast, rhs: &Ast) -> Result<(), ShellError> {
     let stdout_file = file.try_clone()?;
     let stderr_file = file.try_clone()?;
 
-    Command::new(&args[0]).args(&args[1..])
-        .stdout(Stdio::from(stdout_file))
-        .stderr(Stdio::from(stderr_file))
-        .spawn()?
-        .wait()?;
+    thread::spawn(move || {
+        Command::new(&args[0])
+            .args(&args[1..])
+            .stdout(Stdio::from(stdout_file))
+            .stderr(Stdio::from(stderr_file))
+            .spawn()
+    });
 
     Ok(())
 }
