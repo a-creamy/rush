d01f11b10992331936d4d68926a9eef63ae76034
a-creamy <akfain@proton.me>
2025-02-18 15:23:46 +0000

    Bump the version up!

--- src/run/executor.rs
+++ src/run/executor.rs
@@ -1,22 +1,22 @@
-use logical::LogicalType;
-use redirection::RedirectionType;
+mod background;
+mod command;
+mod logical;
+mod pipe;
+mod redirection;
 
 use super::{error::ShellError, node::Ast};
-mod pipe;
-mod command;
-mod redirection;
-mod logical;
-mod background;
+use logical::LogicalType;
+use redirection::RedirectionType;
 
 pub fn execute(node: &Ast) -> Result<(), ShellError> {
     match node {
         Ast::Command(args) => command::execute(args),
         Ast::Pipe(lhs, rhs) => pipe::execute(lhs, rhs),
+        Ast::Background(lhs, rhs) => background::execute(lhs, rhs),
         Ast::AndLogical(lhs, rhs) => logical::execute(lhs, rhs, LogicalType::And),
         Ast::OrLogical(lhs, rhs) => logical::execute(lhs, rhs, LogicalType::Or),
-        Ast::Background(lhs, rhs) => background::execute(lhs, rhs),
-        Ast::OutputRedirection(lhs, rhs) => redirection::execute(lhs, rhs, RedirectionType::Output),
         Ast::InputRedirection(lhs, rhs) => redirection::execute(lhs, rhs, RedirectionType::Input),
+        Ast::OutputRedirection(lhs, rhs) => redirection::execute(lhs, rhs, RedirectionType::Output),
         Ast::OverwriteRedirection(lhs, rhs) => redirection::execute(lhs, rhs, RedirectionType::Overwrite),
         Ast::AppendRedirection(lhs, rhs) => redirection::execute(lhs, rhs, RedirectionType::Append),
         Ast::ErrorRedirection(lhs, rhs) => redirection::execute(lhs, rhs, RedirectionType::Error),
