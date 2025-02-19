4a5670a15577e2d4aada3e5630b5c2fec50b2a20
A-cream <akfain@proton.me>
2025-02-19 20:59:38 +0000

    Small fixes

--- src/run/executor/background.rs
+++ src/run/executor/background.rs
@@ -4,35 +4,32 @@ use std::thread;
 pub fn execute(lhs: &Ast, rhs: &Ast) -> Result<(), ShellError> {
     match lhs {
         Ast::Command(args) => {
-            let handle = spawn_thread(args.clone());
-            drop(handle);
+            let args = args.clone();
+            thread::spawn(move || {
+                if let Err(e) = executor::execute(&Ast::Command(args.clone())) {
+                    eprintln!("rush: {}", e);
+                }
+            });
         }
         Ast::Background(left, right) => {
             execute(left, right)?;
         }
-        _ => {
-            return Err(ShellError::InvalidArgument(
-                "'&' Operator only supports commands".to_string(),
-            ));
+        ast => {
+            executor::execute(ast)?;
         }
     }
 
     match rhs {
         Ast::Command(args) => {
             executor::execute(&Ast::Command(args.clone()))?;
-            Ok(())
         }
-        Ast::Background(left, right) => execute(left, right),
-        _ => Err(ShellError::InvalidArgument(
-            "'&' Operator only supports commands".to_string(),
-        )),
+        Ast::Background(left, right) => {
+            execute(left, right)?;
+        }
+        ast => {
+            executor::execute(ast)?;
+        }
     }
-}
 
-fn spawn_thread(args: Vec<String>) -> thread::JoinHandle<()> {
-    thread::spawn(move || {
-        if let Err(e) = executor::execute(&Ast::Command(args)) {
-            eprintln!("rush: {}", e);
-        }
-    })
+    Ok(())
 }
