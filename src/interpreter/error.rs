2aef83c2efb5ba2842adfc617f0e1b051bb13d2e
a-creamy <akfain@proton.me>
2025-03-23 15:46:26 +0000

    Built in commands support

--- src/interpreter/error.rs
+++ src/interpreter/error.rs
@@ -8,7 +8,29 @@ pub enum ShellError {
     CommandNotFound(String),
     CommandFailure(String, ExitStatus),
     InvalidArgument(String),
+    BicError(String),
     IoError(Error),
+}
+
+#[derive(Debug)]
+pub enum ShellErrorKind {
+    CommandNotFound,
+    CommandFailure,
+    InvalidArgument,
+    BicError,
+    IoError,
+}
+
+impl ShellError {
+    pub fn kind(&self) -> ShellErrorKind {
+        match self {
+            ShellError::CommandNotFound(_) => ShellErrorKind::CommandNotFound,
+            ShellError::CommandFailure(_, _) => ShellErrorKind::CommandFailure,
+            ShellError::InvalidArgument(_) => ShellErrorKind::InvalidArgument,
+            ShellError::BicError(_) => ShellErrorKind::BicError,
+            ShellError::IoError(_) => ShellErrorKind::IoError,
+        }
+    }
 }
 
 impl std::error::Error for ShellError {}
@@ -21,6 +43,7 @@ impl std::fmt::Display for ShellError {
             ShellError::CommandFailure(cmd, exit_status) => {
                 write!(f, "'{}' Failed: Exit Code: {}", cmd, exit_status)
             }
+            ShellError::BicError(msg) => write!(f, "{msg}"),
             ShellError::IoError(e) => write!(f, "IO error: {}", e),
         }
     }
