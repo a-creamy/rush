bd103394bccad16dd853b151febd31a9e36dd7b8
a-creamy <akfain@proton.me>
2025-04-12 21:26:38 +0000

    Cool prompt

--- src/shell.rs
+++ src/shell.rs
@@ -1,10 +1,11 @@
 use super::interpreter::Interpreter;
+use libc::{c_char, gethostname, signal, SIGINT, SIGTSTP, SIG_IGN};
 use std::{
     env,
+    ffi::CStr,
     io::{stdin, stdout, Write},
-    path::PathBuf,
+    path::Path,
 };
-    use libc::{signal, SIGINT, SIGTSTP, SIG_IGN};
 
 struct Shell {
     prompt: String,
@@ -17,15 +18,55 @@ impl Shell {
         }
     }
 
-    fn current_dir(&self) -> PathBuf {
-        let home = env::var("HOME").ok().map(PathBuf::from).unwrap();
-        let current = env::current_dir()
-            .unwrap_or_else(|e| panic!("{e}"))
-            .to_path_buf();
-        if home == current {
-            return PathBuf::from("~");
+    fn get_hostname(&self) -> String {
+        let mut buffer = [0 as c_char; 256];
+
+        let result = unsafe { gethostname(buffer.as_mut_ptr(), buffer.len()) };
+
+        if result == 0 {
+            let c_str = unsafe { CStr::from_ptr(buffer.as_ptr()) };
+            c_str
+                .to_str()
+                .map(|s| s.to_owned())
+                .map_err(|_| "Invalid UTF-8 in hostname".to_string())
+                .expect("Invalid C Str")
         } else {
-            return current;
+            "Failed to get hostname".to_string()
+        }
+    }
+
+    fn shorten_dir(&self) -> String {
+        match env::current_dir() {
+            Ok(path) => {
+                let path_str = if let Some(home) = env::var("HOME").unwrap().into() {
+                    if let Ok(stripped) = path.strip_prefix(&home) {
+                        Path::new("~").join(stripped)
+                    } else {
+                        path
+                    }
+                } else {
+                    path
+                };
+
+                let components: Vec<_> = path_str
+                    .components()
+                    .map(|c| c.as_os_str().to_string_lossy().to_string())
+                    .collect();
+
+                if components.len() >= 2 {
+                    if components[components.len() - 2].starts_with("/") {
+                        return format!("/{}", components[components.len() - 1],);
+                    }
+                    format!(
+                        "{}/{}",
+                        components[components.len() - 2],
+                        components[components.len() - 1]
+                    )
+                } else {
+                    format!("{}", components[components.len() - 1],)
+                }
+            }
+            Err(_) => String::from("Failed to get current directory."),
         }
     }
 
@@ -34,7 +75,8 @@ impl Shell {
         print!(
             "{}",
             self.prompt
-                .replace(r"\w", self.current_dir().to_str().unwrap())
+                .replace(r"\w", self.shorten_dir().as_str())
+                .replace(r"\h", self.get_hostname().as_str())
         );
 
         let _ = stdout().flush();
@@ -51,7 +93,7 @@ pub fn run() {
         signal(SIGTSTP, SIG_IGN);
     }
 
-    let shell = Shell::new(r"\w > ");
+    let shell = Shell::new(r"\h :: \w > ");
     let interpreter = Interpreter::new();
 
     loop {
