03e9fb6d8f586955fe862120ed8084c0b748f764
a-creamy <akramo@tuta.io>
2025-06-29 11:57:41 +0000

    Lexer

---
+++ src/lexer.rs
@@ -0,0 +1,60 @@
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Atomic(String),
+    LogicalAnd,
+}
+
+pub fn lex(input: String) -> Vec<Token> {
+    let mut tokens = Vec::new();
+
+    let mut source = input.chars().peekable();
+    while let Some(ch) = source.peek() {
+        match ch {
+            ' ' | '\t' | '\n' => {
+                source.next();
+            }
+            _ => {
+                let mut atomic = String::new();
+
+                while let Some(c) = source.peek() {
+                    if c.is_alphabetic() {
+                        atomic.push(c.to_owned());
+                        source.next();
+                    } else {
+                        break;
+                    }
+                }
+
+                tokens.push(Token::Atomic(atomic));
+            }
+        }
+    }
+
+    tokens
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_atomic() {
+        let tokens = lex("echo Hello World".to_string());
+        let mut expected_tokens = Vec::new();
+        expected_tokens.push(Token::Atomic("echo".to_string()));
+        expected_tokens.push(Token::Atomic("Hello".to_string()));
+        expected_tokens.push(Token::Atomic("World".to_string()));
+
+        assert_eq!(tokens.len(), expected_tokens.len());
+
+        for i in 0..tokens.len() {
+            assert_eq!(tokens[i], expected_tokens[i]);
+        }
+    }
+
+    #[test]
+    fn test_empty() {
+        let tokens = lex("".to_string());
+        assert!(tokens.is_empty());
+    }
+}
